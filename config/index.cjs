8fa38ffe0f6997ad2fcfa9c4aabfd66a510dbc62
Yang1213233 <140824428+Yang1213233@users.noreply.github.com>
2023-07-28 22:43:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqino6Fyv8EySepSUmP5VjBrShsY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'RqidfoplGcvYeTrptFQgVvdY7tdpSqYZMIdAlYy_Qe8',
+      useTemplateId: 'w9DxuqcBtDGtQGpAurUUvEQDpUBY1c-0IdmbiQ3_siQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '9-28',
       festivals: [
