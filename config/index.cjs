665f91893c50f7c3c695039131d3862e03a0b8a8
Yang1213233 <140824428+Yang1213233@users.noreply.github.com>
2023-07-28 22:19:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小杜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqino6Fyv8EySepSUmP5VjBrShsY',
+      id: 'oqino6MssKfVaCUAr8-ANF0IdEfc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oqino6MssKfVaCUAr8-ANF0IdEfc',
+      useTemplateId: 'ueq0x5dEWRv-ndLq3uIS8fvfcTWvWDUbr8a29XgrovM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '9-28',
       festivals: [
